cb302fd25a18b336cd33ef58f64b657ab73ee71b
hengluwen <867044087@qq.com>
2016-08-01 22:30:12 +0800

    hello github
    i’m coming

---
+++ src/main/java/com/luwen/aux/main/HelloGithub.java
@@ -0,0 +1,16 @@
+package com.luwen.aux.main;
+
+/**
+ * Created by hengluwen on 16/8/1.
+ */
+public class HelloGithub {
+
+    private static void helloGitHub(String username){
+        System.out.print("welcome to github Mr." + username);
+    }
+
+    public static void main(String[] args){
+        helloGitHub("heng");
+    }
+
+}
